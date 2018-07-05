13b6fe5cbf2968e19a6f2e3b36f4d270d696e647
Pablo Satler <pablosatler@gmail.com>
2018-07-05 12:27:28 -0300

    feat: add mock data to NewDeck file

---
+++ components/NewDeck.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+const NewDecks = () => {
+    return (
+        <View>
+            <Text> New Decks </Text>
+        </View>
+    )
+}
+
+export default NewDecks;
