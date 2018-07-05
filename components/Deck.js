d69179c9c3a0cb4e349764274f1016fe6256b6c4
Pablo Satler <pablosatler@gmail.com>
2018-07-05 18:20:58 -0300

    style: add styling to Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Platform } from 'react-native'
+import { lightPurp, white, purple } from '../utils/colors';
 
 
 const Deck = (props) => {
     return (
-        <View >
-            <Text >{props.deckName}</Text>
-            <Text >{props.deckSize}</Text>
+        <View style={deckContainer}>
+            <Text style={deckTitle} >{props.deckName}</Text>
+            <Text style={deckSize} >{props.deckSize} cards</Text>
         </View>
     )
 }
 
 export default Deck;
+
+
+const styles = StyleSheet.create({
+    deckContainer: {
+        // flexDirection: 'row',
+        margin: 5,
+        padding: 20,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        backgroundColor: purple,
+        alignItems: 'center',
+    },
+    deckTitle: {
+        color: white,
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    deckSize: {
+        marginTop: 10,
+        color: white,
+        fontWeight: 'bold',
+        fontSize: 15,
+    }
+})
+const { deckContainer, deckTitle, deckSize } = styles;
