559b5cd463d90ae30a6a0550fc890afb270ca956
Pablo Satler <pablosatler@gmail.com>
2018-07-05 12:25:45 -0300

    feat: add mock data

---
+++ components/Decks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Decks = () => {
+    return (
+        <View>
+            <Text> List of Decks </Text>
+        </View>
+    )
+}
+
+export default Decks;
+
