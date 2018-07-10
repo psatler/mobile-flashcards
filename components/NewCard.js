a6aea53ada0c5c651616bd1c5d55995ec528dacf
Pablo Satler <pablosatler@gmail.com>
2018-07-09 21:25:46 -0300

    feat: create NewCard component skeleton

---
+++ components/NewCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class NewCard extends Component {
+    
+    render() {
+        return (
+            <View>
+                <Text> New Card Component</Text>
+            </View>
+        )
+    }
+}
+
+export default NewCard;
