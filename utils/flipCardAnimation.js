cd8db7646326368347e5c6df0bdc03451ee6d0a4
Pablo Satler <pablosatler@gmail.com>
2018-07-12 16:23:45 -0300

    refactor: code changes in flipCardAnimation file to use ComponendDidMount instead of WillMount (deprecated)

--- utils/flipCardAnimation.js
+++ utils/flipCardAnimation.js
@@ -8,88 +8,97 @@ import {
 } from 'react-native';
 
 export default class AnimatedBasic extends Component {
-  
-  componentWillMount() {
-    this.animatedValue = new Animated.Value(0);
-    this.value = 0;
-    this.animatedValue.addListener(({ value }) => {
-      this.value = value;
-    })
-    this.frontInterpolate = this.animatedValue.interpolate({
-      inputRange: [0, 180],
-      outputRange: ['0deg', '180deg'],
-    })
-    this.backInterpolate = this.animatedValue.interpolate({
-      inputRange: [0, 180],
-    //   outputRange: ['180deg', '360deg'],
-      outputRange: ['180deg', '0deg']
-    })
 
-    //adding this so the back face won't be mirrored on Android devices
-    //https://github.com/facebook/react-native/issues/1973#issuecomment-262059217
-    this.backOpacity = this.animatedValue.interpolate({ 
-        inputRange: [89, 90], 
-        outputRange: [0, 1] 
-    })
-
-    this.frontOpacity = this.animatedValue.interpolate({
-        inputRange: [89, 90],
-        outputRange: [1, 0]
-    })
-
-  }
-  flipCard() {
-    if (this.value >= 90) {
-      Animated.spring(this.animatedValue,{
-        toValue: 0,
-        friction: 8,
-        tension: 10
-      }).start();
-    } else {
-      Animated.spring(this.animatedValue,{
-        toValue: 180,
-        friction: 8,
-        tension: 10
-      }).start();
+    state = {
+        animatedValue: new Animated.Value(0),
+        listenerValue: 0,
     }
 
-  }
-  
-  render() {
-    const frontAnimatedStyle = {
-      transform: [
-        { rotateY: this.frontInterpolate}
-      ],
-      opacity: this.frontOpacity, //adding this so it won't be mirrored on Android devices
+    componentDidMount(){
+        const { animatedValue, listenerValue } = this.state;
+
+        //creating a listener to track values changes
+        animatedValue.addListener( ( { value } ) => {
+            this.setState({
+                listenerValue: value,
+            })
+        })
     }
-    const backAnimatedStyle = {
-      transform: [
-        { rotateY: this.backInterpolate }
-      ],
-      opacity: this.backOpacity, //adding this so it won't be mirrored on Android devices
+
+    flipCard = () => {
+        const { animatedValue, listenerValue } = this.state;
+
+        if(listenerValue >= 90){
+            Animated.spring(animatedValue,{
+                toValue: 0,
+                friction: 8,
+                tension: 10
+              }).start();
+        } 
+        else {
+            Animated.spring(animatedValue,{
+                toValue: 180,
+                friction: 8,
+                tension: 10
+              }).start();
+        }
     }
-    
-    return (
-      <View style={styles.container}>
-        <View>
-          <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
-            <Text style={styles.flipText}>
-              This text is flipping on the front.
-            </Text>
-          </Animated.View>
-          <Animated.View style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
-            <Text style={styles.flipText}>
-              This text is flipping on the back.
-            </Text>
-          </Animated.View>
-        </View>
-        <TouchableOpacity onPress={() => this.flipCard()}>
-          <Text>Flip!</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+
+    render() {
+        const { animatedValue, listenerValue } = this.state;
+
+        const frontAnimatedStyle = {
+          transform: [
+            { rotateY: animatedValue.interpolate({
+                inputRange: [0, 180],
+                outputRange: ['0deg', '180deg'],
+              })
+            }
+          ],
+          //adding opacity so the back face won't be mirrored on Android devices
+        //https://github.com/facebook/react-native/issues/1973#issuecomment-262059217
+          opacity: animatedValue.interpolate({ //adding this so it won't be mirrored on Android devices
+                inputRange: [89, 90],
+                outputRange: [1, 0]
+            }), 
+        }
+        const backAnimatedStyle = {
+          transform: [
+            { rotateY: animatedValue.interpolate({
+                inputRange: [0, 180],
+              //   outputRange: ['180deg', '360deg'],
+                outputRange: ['180deg', '0deg']
+              }) 
+            }
+          ],
+          opacity: animatedValue.interpolate({  //adding this so it won't be mirrored on Android devices
+                inputRange: [89, 90], 
+                outputRange: [0, 1] 
+            }), 
+        }
+        
+        return (
+          <View style={styles.container}>
+            <View>
+              <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+                <Text style={styles.flipText}>
+                  This text is flipping on the front.
+                </Text>
+              </Animated.View>
+              <Animated.View style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
+                <Text style={styles.flipText}>
+                  This text is flipping on the back.
+                </Text>
+              </Animated.View>
+            </View>
+            <TouchableOpacity onPress={() => this.flipCard()}>
+              <Text>Flip!</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      }
+    }
+
 
 const styles = StyleSheet.create({
   container: {
