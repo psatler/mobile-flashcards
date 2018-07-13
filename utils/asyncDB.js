1cbf2e745677268a308f2e0266b689a06bfa0539
Pablo Satler <pablosatler@gmail.com>
2018-07-13 18:01:39 -0300

    chore: change addCardToDeck implementation

--- utils/asyncDB.js
+++ utils/asyncDB.js
@@ -13,12 +13,12 @@ export const getDecks = async () => {
     try {
         let value = await AsyncStorage.getItem(FLASHCARDS_DECKS_KEY);
         if( value !== null){ //we have data
-            // console.log('decks', value);
             let parsedObj = JSON.parse(value); //parsing back to object
             return parsedObj;
         } else { //if not, we set initial state
-            console.log('setting initial state');
-            setInitialData(); //defined above
+            // console.log('setting initial state');
+            // setInitialData(); //defined above
+            return {} 
         }
     } catch (err) {
         console.log('There was an error retrieving decks from local DB: ', err);
@@ -39,56 +39,28 @@ export const saveDeckTitle = (keyTitle, deckObj) => {
 export const addCardToDeck = (deckTitle, card) => {
     const key = deckTitle;
 
-    return AsyncStorage.getItem(FLASHCARDS_DECKS_KEY)
-        .then( (results) => {
-            const allDecks = JSON.parse(results);
-            const deck = allDecks[key];
-            // console.log('addCardToDeck- BEFORE', deck.questions)
-            deck.questions.push(card)
-            // console.log('addCardToDeck', deck.questions)
-            // console.log('allDecks', allDecks)
+    return getDecks().then( (allDecks) => { //it comes parsed to json already
+        const deck = allDecks[key];
+        deck.questions.push(card);
 
-            AsyncStorage.setItem(FLASHCARDS_DECKS_KEY, JSON.stringify(allDecks)); //updating 
+        AsyncStorage.setItem(FLASHCARDS_DECKS_KEY, JSON.stringify(allDecks)); //updating 
 
-        })
-        // const key = deckTitle.split(' ').join('');
-
+    })
 }
 
-// 
-
-// const checkIfDataExists = (results) => {
-//     return results === null
-//       ? setInitialData()
-//       : JSON.parse(results)
-//   }
-
-// export const retrieveDataFromAsyncStorage = () => {
 
 //     return AsyncStorage.getItem(FLASHCARDS_DECKS_KEY)
-//         .then(checkIfDataExists)
+//         .then( (results) => {
+//             const allDecks = JSON.parse(results);
+//             const deck = allDecks[key];
+//             // console.log('addCardToDeck- BEFORE', deck.questions)
+//             deck.questions.push(card)
+//             // console.log('addCardToDeck', deck.questions)
+//             // console.log('allDecks', allDecks)
+
+//             AsyncStorage.setItem(FLASHCARDS_DECKS_KEY, JSON.stringify(allDecks)); //updating 
+
+//         })
+        
 // }
 
-
-
-// _setValue = async () => {
-//     try {
-//       await AsyncStorage.setItem(STORAGE_KEY, 'true');
-//     } catch (error) { // log the error
-//     }
-//   };
-
-//   _loadInitialState = async () => {
-//     try {
-//       let value = await AsyncStorage.getItem(STORAGE_KEY);
-//       if (value === 'true'){
-//         this.setState({loaded: 'true'});
-//       } else {
-//         this.setState({loaded: 'false'});
-//         this._setValue();
-//       }
-//     } catch (error) {
-//       this.setState({loaded: 'false'});
-//       this._setValue();
-//     }
-//   };
