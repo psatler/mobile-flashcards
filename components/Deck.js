f5915c02903de7b302f3e6aa01f2afb069ba6adc
Pablo Satler <pablosatler@gmail.com>
2018-07-13 17:38:44 -0300

    style: change styling of Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Platform } from 'react-native'
-import { lightPurp, white, purple } from '../utils/colors';
+import { Text, View, StyleSheet, Platform, Image } from 'react-native'
+import { lightPurp, white, purple, lightBlue, red } from '../utils/colors';
+import { Entypo } from '@expo/vector-icons'
 
+const AVATAR_IMAGE_HEIGHT = 40
+// const AVATAR_IMAGE_WIDTH = 40
 
 const Deck = (props) => {
     return (
         <View style={deckContainer}>
-            <Text style={deckTitle} >{props.deckName}</Text>
-            <Text style={deckSize} >{props.deckSize} cards</Text>
+            <View style={ styles.avatar} >
+                <Image 
+                    source={ require('../assets/reactNativeWhiteBackground.png')}
+                    style={styles.image}
+                />
+            </View>
+            <View style={ styles.deckInfo} >
+                <Text style={deckTitle} >{props.deckName}</Text>
+                <Text style={deckSize} >{props.deckSize} cards</Text>
+            </View>
+            <View style={ styles.removeButton} >
+                <Entypo name='dots-three-vertical' size={30}  />
+            </View>
         </View>
     )
 }
@@ -17,23 +31,53 @@ export default Deck;
 
 const styles = StyleSheet.create({
     deckContainer: {
-        // flexDirection: 'row',
+        // flex: 1, //uncomment this so in the details screen it takes the whole screen space
+        flexDirection: 'row',
         margin: 5,
-        padding: 20,
+        padding: 5,
         borderRadius: Platform.OS === 'ios' ? 16 : 2,
-        backgroundColor: purple,
+        borderColor: lightBlue,
+        borderWidth: 1,
+        minHeight: 80,
+        // backgroundColor: white,
+        // alignItems: 'center',
+    },
+    image: {
+        flex: 1,
+        width: null,
+        height: null,
+    },
+    avatar: {
+        flex: 0.5,
+        // backgroundColor: purple,
+        // height: AVATAR_IMAGE_HEIGHT,
+        // width: AVATAR_IMAGE_HEIGHT,
+        borderRadius: AVATAR_IMAGE_HEIGHT/2, 
+        borderColor: lightBlue,
+        borderWidth: 1,
+        overflow: 'hidden'
+
+    },
+    deckInfo: {
+        flex: 2,
         alignItems: 'center',
     },
     deckTitle: {
-        color: white,
+        // color: white,
         fontWeight: 'bold',
         fontSize: 20,
     },
     deckSize: {
         marginTop: 10,
-        color: white,
+        // color: white,
         fontWeight: 'bold',
         fontSize: 15,
+    }, 
+    removeButton: { 
+        flex: 0.5, 
+        // backgroundColor: purple,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
 const { deckContainer, deckTitle, deckSize } = styles;
