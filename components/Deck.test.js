59c24f15e46b08b61a8efa063562040558cc68a5
Pablo Satler <pablosatler@gmail.com>
2018-07-19 16:38:51 -0300

    test: try shallow render Deck component

--- components/Deck.test.js
+++ components/Deck.test.js
@@ -1,11 +1,32 @@
+import React from 'react';
+import Deck from './Deck';
+import { shallow } from 'enzyme';
 import { data } from '../utils/mockData'
 
-
+import renderer from 'react-test-renderer';
 
 
 describe('[Component] Deck', () => {
+    const singleDeck = data['JavaScript'];
+
+    it('renders correctly', () => {
+        const rendered = renderer.create(
+            <Deck 
+                deckName={singleDeck.title}
+                deckSize={singleDeck.questions.length}
+                imageURI={singleDeck.image}
+            />).toJSON();
+        expect(rendered).toMatchSnapshot();
+    });
+
 
     it('shallow renders correctly', () => {
-        
+        const wrapper = shallow(
+            <Deck 
+                deckName={singleDeck.title}
+                deckSize={singleDeck.questions.length}
+                imageURI={singleDeck.image}
+            />);
+        expect(wrapper).toMatchSnapshot();
     })
-})
+});
