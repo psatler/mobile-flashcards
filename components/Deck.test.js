48156390b46437fac88911c105924c2633b22ed3
Pablo Satler <pablosatler@gmail.com>
2018-07-19 19:04:39 -0300

    test: add shallow render test to Deck Component

--- components/Deck.test.js
+++ components/Deck.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Deck from './Deck';
-import { shallow } from 'enzyme';
 import { data } from '../utils/mockData'
 
 import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 
 describe('[Component] Deck', () => {
@@ -19,14 +19,27 @@ describe('[Component] Deck', () => {
         expect(rendered).toMatchSnapshot();
     });
 
-
-    it('shallow renders correctly', () => {
-        const wrapper = shallow(
+    it('renders user shallow correctly', () => {
+        const renderer = new ShallowRenderer();
+        renderer.render(
             <Deck 
                 deckName={singleDeck.title}
                 deckSize={singleDeck.questions.length}
                 imageURI={singleDeck.image}
             />);
-        expect(wrapper).toMatchSnapshot();
-    })
+        const tree = renderer.getRenderOutput();
+        expect(tree).toMatchSnapshot();
+      });
 });
+
+
+/**
+ * $ 
+ *  npm install --save-dev react-dom react-addons-test-util
+    npm WARN notice Due to a recent security incident, all user tokens have been invalidated. Please see https://status.npmjs.org/incidents/dn7c1fgrr7ng for more details. To generate a new token, visit https://www.npmjs.com/settings/~/tokens or run "npm login".
+    npm ERR! code E404
+    npm ERR! 404 Not Found: react-addons-test-util@latest
+
+    npm ERR! A complete log of this run can be found in:
+    npm ERR!     C:\Users\Pablo\AppData\Roaming\npm-cache\_logs\2018-07-19T21_16_41_344Z-debug.log
+ */
