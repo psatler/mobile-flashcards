b50473a66b47536c462f29b4bd01e885e9a534c4
Pablo Satler <pablosatler@gmail.com>
2018-07-20 17:52:20 -0300

    test: mock navigation.navigate method to test ShowResult component

--- components/ShowResult.test.js
+++ components/ShowResult.test.js
@@ -21,6 +21,7 @@ describe('[Component] Deck', () => {
     deckNoCards.questions = []
     // const deckWithNoCards = {"newDeck": {"questions": [], "title": "new Deck"}} 
 
+    //passing a deck with cards
     it('should show a message saying the score and two buttons: restart quiz and back to deck', () => {
         const score = 0;
         const rendered = renderer.create(
@@ -29,6 +30,7 @@ describe('[Component] Deck', () => {
         expect(rendered).toMatchSnapshot();
     });
 
+    //passing a deck without cards (empty)
     it('should show a message saying there is no cards inside deck and display only one button: back to deck', () => {
         const score = 0;
         const renderer = new ShallowRenderer();
@@ -43,22 +45,29 @@ describe('[Component] Deck', () => {
         const score = 0;
         const wrapper = shallow(
             <ShowResult score={score} singleDeck={deck} restartFunc={restartQuiz} />
-        )
+        );
 
-    })
+        wrapper.find('TouchableOpacity').first().props().onPress()
+        expect(restartQuiz).toHaveBeenCalled();
 
+        // console.log(wrapper.find('TouchableOpacity').first().props().onPress())
 
-    xit('should show a call the restart button', () => {
-        const score = 0;
-        let rendered = renderer.create(
-            <ShowResult score={score} singleDeck={deck} restartFunc={restartQuiz} />
-        ).getInstance();
-        // expect(rendered.).toBeCalled();
-        // expect(rendered.props.restartFunc).toBeTruthy();
-        // expect(rendered.props.restartFunc).toBeCalled();
-        console.log(rendered.props.restartFunc)
-        // tree.find('TouchableOpacity').forEach( button => console.log(button))
-        // expect(tree).toMatchSnapshot();
     });
+
+    it('should show a call the back to deck button', () => {
+        const score = 0;
+        const navigation = { navigate: jest.fn() }; //mocking navigate function of react navigation and passing it as props to the component that uses it
+
+        const wrapper = shallow(
+            <ShowResult score={score} singleDeck={deck} restartFunc={restartQuiz} navigation={navigation} />
+        );
+
+        wrapper.find('TouchableOpacity').last().props().onPress()
+        expect(navigation.navigate).toHaveBeenCalled();
+        
+        // console.log(wrapper.find('TouchableOpacity').last().props().onPress())
+
+    });
+
 
 });
