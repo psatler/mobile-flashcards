141f1c758b3a9f9cc643410e970ef364ec3c84a1
Pablo Satler <pablosatler@gmail.com>
2018-07-21 19:12:03 -0300

    test: try to test inserting some text into deck title field

--- components/NewDeck.test.js
+++ components/NewDeck.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NewDeck } from './NewDeck'
 import { ImagePicker, Permissions } from 'expo'
+import { addDeck } from '../actions'
 import renderer from 'react-test-renderer';
 // import ShallowRenderer from 'react-test-renderer/shallow';
 
@@ -10,7 +11,32 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 import { shallow } from 'enzyme'
 
+
+jest.mock('expo', ()=>({
+    Permissions: {
+    //    askAsync: jest.fn(() => ({ status: 'granted' }))
+       askAsync: jest.fn(() => {
+           return new Promise( (resolve, reject) => {
+               return resolve({
+                   status: 'granted',
+               })
+           })
+       })
+    },
+    ImagePicker: {
+        launchImageLibraryAsync: jest.fn( () => ({
+            allowsEditing: true,
+            aspect: [4, 3],
+        })),
+    
+    },
+  }))
+
 describe('[Component] New Deck', () => {
+
+    afterEach( () => {
+        // jest.resetAllMocks();
+    })
 
     it('shallow render correctly', () => {
         const wrapper = shallow(<NewDeck />);
@@ -32,16 +58,33 @@ describe('[Component] New Deck', () => {
         // console.log(NewDeck.prototype)
     });
 
-    xit('should call askPermission method', async () => {
-        jest.mock('expo', ()=>({
-            Permissions: {
-               askAsync: jest.fn().mockImplementationOnce(() => ({ status: 'granted' }))
-            }
-          }))
-
+    it('should call askPermission method, which call pickImage', async () => {
         const wrapper = shallow(<NewDeck />);
-        const res = await wrapper.instance().askPermission();
-        console.log('res', res)
+        await wrapper.instance().askPermission();
+    });
+
+    it('should call pickImage method', async () => {
+        const wrapper = shallow(<NewDeck />);
+        await wrapper.instance().pickImage()
+        // console.log(ImagePicker.launchImageLibraryAsync);
+        ImagePicker.launchImageLibraryAsync = jest.fn(() => ({
+                cancelled: true,
+        }))
+        // console.log('after ',ImagePicker.launchImageLibraryAsync);
+        await wrapper.instance().pickImage()
+    });
+
+    xit('should call enter if statement', () => {
+        const wrapper = shallow(<NewDeck />);
+        // const textInput = wrapper.find('TextInput').instance().value = 'bosta'
+        wrapper.find('TextInput').getDOMNode().value = 'bosta'
+        // textInput.value = 'bosta'
+        // console.log('after ',textInput);
+
+        // wrapper.instance().submitDeck();
+        expect(wrapper.state("input")).toEqual('bosta')
     })
+
+
     
 });
